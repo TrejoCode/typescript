622dde93469215a0e3afa3dc861780c3a5a02ed5
Sergio Trejo <alejandrotrejocode@gmail.com>
2020-08-20 18:31:03 -0500

    📚 Arreglos

---
+++ 04-arrays/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * @description Arreglos en TS
+*/
+
+// Explícito
+let users: string[];
+users = ['Hola', 'Mundo'];
+
+const values: string[] = ['Hola', 'Mundo'];
+
+
+// Implícito
+
+const secondNames: Array<string> = ['Hola', 'Mundo'];
+
+let thirdNames: Array<string>;
+thirdNames = ['Hola', 'Mundo'];
